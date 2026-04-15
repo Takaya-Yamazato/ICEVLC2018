88528dcd97e7653e9ba7b77052ca4e7f21cf92d0
Takaya YAMAZATO <Takaya-Yamazato@users.noreply.github.com>
2026-04-15 21:33:08 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yamazato.nuee.nagoya-u.ac.jp/icevlc2018', // サイトのURLを設定
+  base: '/icevlc2018', // リポジトリ名に合わせて設定
+
   integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()]
   }
+  
 });
+
